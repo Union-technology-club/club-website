1f5046ede384e8899d7e4e158fac090a65ca8fd9
Keith Wade <keawade@gmail.com>
2015-09-14 00:19:28 -0500

    Manually updated dates / rolled back changes
    Needed to update the dates and didn't have time to figure out the Mongo
    update that didn't work out of box.

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-var express = require('express');
-var hbs = require('hbs');
-var hbsutils = require('hbs-utils')(hbs);
-var bodyParser = require('body-parser');
-var google = require('googleapis');
-var nodemailer = require('nodemailer');
-var gmailauth = require('./auth.json');
-var mongoose = require('mongoose');
-
-var OAuth2 = google.auth.OAuth2;
-//var oauth2Client = new OAuth2(
-//var gmailauth = require('./auth.js');
+var express = require('express'),
+    hbs = require('hbs'),
+    hbsutils = require('hbs-utils')(hbs),
+    bodyParser = require('body-parser'),
+    google = require('googleapis');
 
 // Initialize our express application
 var app = express();
@@ -33,44 +26,11 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-//Connect mongoose to mongodb
-app.set('host', process.env.IP || 'localhost');
-app.set('name', 'TTC');
-mongoose.connect('mongodb://' + app.get('host') + '/' + app.get('name'));
-
-var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: gmailauth
-});
-
 app.get('/', function(req,res,next){
     res.render('index', {
         title: 'Tech & Tabletop Club'
     })
 });
-app.post('/email', function(req,res,next){
-  console.log(req.body);
-  var mailOptions = {
-      from: 'UnionTTC <uctechtabletop@gmail.com>',
-      to: 'uctechtabletop@gmail.com',
-      subject: 'Feedback from website',
-      html: '<ul>'+
-              '<li>From: '+req.body.email+'</li>'+
-              '<li>Message: '+req.body.message+'</li>'+
-            '</ul>'
-  };
-  transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-      return console.log(error);
-    }
-    console.log('Message sent: ' + info.response);
-    //If you felt like it, you could do fancy renders here, or a thank
-    //you page or something.
-    res.redirect('/');
-  });
-});
-
-app.use("/library", require('./routes/library'))
 
 var server = app.listen(app.get('port'), app.get('ip'), function() {
     var address = server.address();
